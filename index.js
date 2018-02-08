bb48e093c9ce9c3e558d0ad5e99f5589ea2ba99d
Tiffany Schureman <Tiffany@Tiffanys-MacBook-Air.local>
2018-02-08 13:58:08 -0600

    step one and two done

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require ('body-parser');
+const cors = require ('cors');
+const massive = require ('massive');
+require('dotenv').config()
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+massive (process.env.CONNECTION_STRING).then(dbInstance => app.set ('db', dbInstance));
+
+const port = process.env.PORT || 3000;
+app.listen (port, () => {console.log(`Server listening on port ${port}.`);});
