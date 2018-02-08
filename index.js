b3a60813106907f7efa933bed1e368fea0b4f2c3
Tiffany Schureman <Tiffany@Tiffanys-MacBook-Air.local>
2018-02-08 14:57:16 -0600

    step 5 done endpoints

--- index.js
+++ index.js
@@ -10,5 +10,11 @@ app.use(bodyParser.json());
 app.use(cors());
 massive (process.env.CONNECTION_STRING).then(dbInstance => app.set ('db', dbInstance));
 
+app.post('/api/product', products_controller.create);
+app.get('/api/products', products_controller.getAll);
+app.get('/api/product/:id', products_controller.getOne);
+app.put('/api/product/:id', products_controller.update);
+app.delete('/api/product/:id', products_controller.delete);
+
 const port = process.env.PORT || 3000;
 app.listen (port, () => {console.log(`Server listening on port ${port}.`);});
